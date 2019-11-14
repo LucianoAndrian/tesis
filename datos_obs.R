f6a883431b6c5b95273285479121e2e949e6a27d
V <44793625+LucianoAndrian@users.noreply.github.com>
2019-11-14 18:09:03 -0300

    intento de grafico

--- datos_obs.R
+++ datos_obs.R
@@ -43,3 +43,55 @@ for( i in 1:4){
 }
  
 # probar graficos
+library(ncdf4)
+library(maps)
+library(ncdf4)
+require(fields)
+require(mapdata)
+library(ggplot2)
+library(metR)
+library(RColorBrewer)
+library(mapproj)
+
+
+lon = 23
+lat = 30
+
+value = array(estaciones_prom[i], dim = length(lon2)*length(lat2))
+data = matrix(data = NA, nrow = length(lon2)*length(lat2), ncol = 3)
+
+l=0
+while(l<length(lon2)*length(lat2)){
+  data[seq(l:l+length(lon2)),1]<-lon2
+  l=l+length(lon2)
+}
+
+for(j in 1:length(lat2)){
+  lat_v = array(lat2[j],dim=length(lon2))
+  data[(length(lon2)*j-(length(lon2)-1)):(j*length(lon2)),2]<-lat_v
+} 
+
+data[,3]<-value
+error<-as.data.frame(data)
+
+colnames(error)<-c("lon", "lat", "temp")
+
+error[which(error$lon>180),][,1]<-error[which(error$lon>180),][,1]-360  
+
+mapa <- map_data("world", regions = c("Brazil", "Uruguay", "Argentina", "French Guiana", "Suriname", "Colombia", "Venezuela",
+                                      "Bolivia", "Ecuador", "Chile", "Paraguay", "Peru", "Guyana", "Panama", "Costa Rica", "Nicaragua"), 
+                 colour = "black")
+# veeeer
+g <- ggplot() + theme_minimal()+
+  xlab("Longitud") + ylab("Latitud") + 
+  theme(panel.border = element_blank(), panel.grid.major = element_line(colour = "grey"), panel.grid.minor = element_blank())+
+  geom_contour_fill(data=error,aes(x = lon, y= lat, z = temp),alpha=1,na.fill=(-10000))+
+  scale_fill_gradientn(limits=c(-100,100),name="°C",colours=(brewer.pal(n=11,"Spectral")),na.value = "white")+
+  geom_polygon(data=mapa, aes(x=long,y=lat, group =group),fill = NA, color = "black") +coord_map("stereographic", orientation = c(-35, -56, 0))+
+  ggtitle(paste("prueba", " - " , "prueba", sep = ""))+
+  scale_x_continuous(limits = c(-90, -30))+
+  theme(axis.text.y   = element_text(size=14), axis.text.x   = element_text(size=14), axis.title.y  = element_text(size=14),
+        axis.title.x  = element_text(size=14), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+        panel.border = element_rect(colour = "black", fill=NA, size=3),
+        panel.ontop = TRUE,
+        plot.title = element_text(hjust=0.5))
