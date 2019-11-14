e4247c7c10e093ac339704060d568c69844ca44a
V <44793625+LucianoAndrian@users.noreply.github.com>
2019-11-14 17:36:00 -0300

    sin datos faltantes

--- datos_obs.R
+++ datos_obs.R
@@ -1,30 +1,45 @@
 rm(list=ls()) 
-setwd("/home/auri/Facultad/")
+require(here)
+#setwd("/home/auri/Facultad/")
+PATH <- "/home/luciano/V"
+setwd(PATH)
 library(ncdf4)
-path<-"/home/auri/Descargas/"
+#path<-"/home/auri/Descargas/"
 
-tref = nc_open(paste(path, "tref_monthly_nmme_ghcn_cams.nc", sep = "/"))
+tref = nc_open(paste(PATH, "tref_monthly_nmme_ghcn_cams.nc", sep = "/"))
 names(tref$var)
 temp = ncvar_get(tref, "tref")
 
-# la serie desde  mayo del '82 hasta agosto 2013
-# faltan feb '86, dic '91, sep '97, jul 2003 y may 2009 cada vez que los dias alcanzan el 30 salta un mes. ver ncview
-# los dias se mueven 1, 1, 2, 2, 3, 3. arranca con el mayo '82 con 11
-# saltea meses en los tiempos 45, 114, 182, 251 y 320
+# serie desde enero '82 -> noviembre 2012 
+# NO FALTAN MESES, SOLO LO LEE RARO NCVIEW ---> USAR NCDUMP POR TERMINAL
+# TOMAR DESDE MARZO DEL '82 HASTA NOV DEL 2011?? ---> uso feb del 2012 asi me quedan misma cantidad de estaciones.  ES IMPORTANTE O DA IGUAL??
 lat = ncvar_get(tref, "Y")
 lon = ncvar_get(tref, "X")
 
-# tomo desde Dec '82 hasta NOV '2012 
-temp = temp[,,8:362] # cambian los tiempos que se salta.
+temp = temp[,,3:371] 
 
+lon2 = c(which(lon==275):which(lon==330))
+lat2 = c(which(lat==-60):which(lat==15))
 
-temp_m = array(NA, dim = c(360, 181, 30, 12)) # 3 dim = anios, 4dim = meses
+temp_estaciones = array(NA, dim = c(length(lon2), length(lat2), 30, 12)) # CON ESTA DIMENCION YA CUMPLE LO DE ARRIBA.
 
 for(j in 1:12){
   for (i in 0:29){
-    if(j+12*i==45-6){
-      temp_m[,,1+i,j] = NA
-    } else if(j == 1 & i == 114)
-    temp_m[,,1+i,j] = temp[, ,j+12*i]
+    temp_estaciones[,,1+i,j] = temp[lon2, lat2, j+12*i]
   }
 }
+
+
+estaciones_p_a = array(NA, dim = c(length(lon2), length(lat2), 30, 4))
+for(i in 1:9){
+  estaciones_p_a[,,,i-ceiling(i/2)] = apply(temp_estaciones[,,,1:i+3], c(1,2,3), mean)
+  i = i + 3
+}
+
+estaciones_prom = array(NA, dim = c(length(lon2), length(lat2), 4))
+
+for( i in 1:4){
+  estaciones_prom[,,i] = apply(estaciones_p_a[,,,i], c(1,2), mean)
+}
+ 
+# probar graficos
