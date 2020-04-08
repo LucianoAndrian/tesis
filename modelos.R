e8ca32882b270a9acb6793f2941db66dc7ec0fda
Luciano Andrian <luciano.andrian@vegeta.cima.fcen.uba.ar>
2020-04-08 19:13:03 -0300

    actualizacion graficos con mapa_sig

--- modelos.R
+++ modelos.R
@@ -47,7 +47,7 @@ for(i in 1:length(nombres2)){
   # SD - T
   mapa(lista = v[[2]], titulo = paste(" SD Temperatura 1982 - 2010 - ", nombres2[i], sep = ""), nombre_fig = paste("SD-temp_",nombres2[i], sep = ""), escala = c(0,3) 
        , label_escala = "°C", resta = 0, brewer = "YlOrRd", revert = "no", niveles = 9 
-       , contour = "si", lon2, lat2, seq(0, 3, by = 0.5), seq(0, 3, by = 0.5), seq(0, 3, by = 0.5), "/salidas/ensemble/")
+       , contour = "si", lon2, lat2, seq(0, 3, by = 0.5), seq(0, 3, by = 0.5), "/salidas/ensemble/")
   
   # PP
   
@@ -58,7 +58,7 @@ for(i in 1:length(nombres2)){
   # SD
   
   mapa(lista = v[[4]], titulo = paste("SD Precipitaciòn 1982 - 2010 - ", nombres2[i], sep = ""), nombre_fig = paste("SD-pp_",nombres2[i], sep = ""), escala = c(0, 150) 
-       , label_escala = "mm", resta = 0, brewer = "YlGn", revert = "no", niveles = 9
+       , label_escala = "mm", resta = 0, brewer = "YlGnBu", revert = "no", niveles = 9
        , contour = "si", lon2, lat2, seq(0, 150, by = 25), seq(0, 150, by = 25), "/salidas/ensemble/")
  
 }
@@ -85,30 +85,34 @@ letras = c(as.character("\u03b1"), as.character("\u03B2"), as.character("\u194")
 # testeos cocientes #
 
 #source("tests.R")
-sig_temp = test_cos(ss_temp)
-sig_pp = test_cos(ss_pp)
+
+sig_temp = test_cos(ss_temp, ensemble_total = "si", nomodel_selec = "no", no_model = "no")
+sig_pp = test_cos(ss_pp, ensemble_total = "si", nomodel_selec = "no", no_model = "no")
+
+
+# esta OK.
 
 
 #source("mapa_sig.R")
 for(i in 6:9){  
   
-  mapa_sig(lista = ss_temp[[i]]*sig_temp[[i-5]], lista2 = sig_temp[[i-5]], titulo = paste("T - Fraccion de TSS representada por SS", letras[i-5] , by = "")  , nombre_fig = paste("temp_ss", letras[i-5], sep = ""), escala = c(0,1) 
-       ,label_escala = "", resta = 0, brewer = "OrRd", revert = "no", niveles = 5, contour = "si", lon2, lat2, seq(0, 1, by = 0.2),"/salidas/ensemble/anova/")
+  mapa_sig(lista = ss_temp[[i]]*mask_arr, lista2 = sig_temp[[i-5]], titulo = paste("T - Fraccion de TSS representada por SS", letras[i-5] , by = "")  , nombre_fig = paste("temp_ss", letras[i-5], sep = ""), escala = c(0,1) 
+       ,label_escala = "", resta = 0, brewer = "OrRd", revert = "no", niveles = 9, contour = "si", lon2, lat2, seq(0, 1, by = 0.2), seq(0, 1, by = 0.1), 0.7, "/salidas/ensemble/anova/")
   
-  
-  mapa_sig(lista = ss_pp[[i]]*sig_pp[[i-5]], lista2 = sig_pp[[i-5]], titulo = paste("PP - Fraccion de TSS representada por SS", letras[i-5] , by = "")  , nombre_fig = paste("pp_ss", letras[i-5], sep = ""), escala = c(0,1) 
-       ,label_escala = "", resta = 0, brewer = "PuBuGn", revert = "no", niveles = 5, contour = "si", lon2, lat2, seq(0, 1, by = 0.2),"/salidas/ensemble/anova/")
+
+  mapa_sig(lista = ss_pp[[i]]*mask_arr, lista2 = sig_pp[[i-5]], titulo = paste("PP - Fraccion de TSS representada por SS", letras[i-5] , by = "")  , nombre_fig = paste("pp_ss", letras[i-5], sep = ""), escala = c(0,1) 
+       ,label_escala = "", resta = 0, brewer = "PuBuGn", revert = "no", niveles = 9, contour = "si", lon2, lat2, seq(0, 1, by = 0.2), seq(0, 1, by = 0.2), 0.7,"/salidas/ensemble/anova/")
   
 }
 
 
 # ssg como otra escala # 0--> 0.2 de a 0.04
 
-mapa_sig(lista = ss_temp[[8]]*sig_temp[[8-5]], lista2 = sig_temp[[8-5]], titulo = paste("T - Fraccion de TSS representada por SS", letras[8-5] , by = "")  , nombre_fig = paste("esc_temp_ss", letras[8-5], sep = ""), escala = c(0,0.2) 
-     ,label_escala = "", resta = 0, brewer = "OrRd", revert = "no", niveles = 5, contour = "si", lon2, lat2, seq(0, 0.2, by = 0.04),"/salidas/ensemble/anova/")
+mapa_sig(lista = ss_temp[[8]]*mask_arr, lista2 = sig_temp[[8-5]], titulo = paste("T - Fraccion de TSS representada por SS", letras[8-5] , by = "")  , nombre_fig = paste("esc_temp_ss", letras[8-5], sep = ""), escala = c(0,0.2) 
+         ,label_escala = "", resta = 0, brewer = "OrRd", revert = "no", niveles = 9, contour = "si", lon2, lat2, seq(0, 0.2, by = 0.02), seq(0, 0.2, by = 0.01), 0.7, "/salidas/ensemble/anova/")
 
-mapa_sig(lista = ss_pp[[8]]*sig_pp[[8-5]], lista2 = sig_pp[[8-5]], titulo = paste("PP - Fraccion de TSS representada por SS", letras[8-5] , by = "")  , nombre_fig = paste("esc_pp_ss", letras[8-5], sep = ""), escala = c(0,0.2) 
-     ,label_escala = "", resta = 0, brewer = "PuBuGn", revert = "no", niveles = 5, contour = "si", lon2, lat2, seq(0, 0.2, by = 0.04),"/salidas/ensemble/anova/")
+mapa_sig(lista = ss_pp[[8]]*mask_arr, lista2 = sig_pp[[8-5]], titulo = paste("PP - Fraccion de TSS representada por SS", letras[8-5] , by = "")  , nombre_fig = paste("esc_pp_ss", letras[8-5], sep = ""), escala = c(0,0.2) 
+     ,label_escala = "", resta = 0, brewer = "PuBuGn", revert = "no", niveles = 9, contour = "si", lon2, lat2, seq(0, 0.2, by = 0.02), seq(0, 0.2, by = 0.01), 0.7,"/salidas/ensemble/anova/")
 
 
 ##-------------------------------------------------------------------------PREDICTIBILIDAD----------------------------------------------------------------##
@@ -122,15 +126,15 @@ mapa_sig(lista = ss_pp[[8]]*sig_pp[[8-5]], lista2 = sig_pp[[8-5]], titulo = past
 pp = pp_test(ss_temp, ss_pp)
 
 
-mapa_sig(lista = pp[[1]]*pp[[2]], lista2 = pp[[2]], titulo = paste("Predictibilidad - T",  by = "")  , nombre_fig = paste("pred_temp"), escala = c(0,1) 
-         ,label_escala = "", resta = 0, brewer = "YlOrRd", revert = "no", niveles = 6, contour = "si", lon2, lat2, seq(0, 1, by = 0.2),"/salidas/ensemble/anova/pred/")
+mapa_sig(lista = pp[[1]]*mask_arr, lista2 = pp[[2]], titulo = paste("Predictibilidad - T",  by = "")  , nombre_fig = paste("pred_temp"), escala = c(0, 1) 
+         ,label_escala = "", resta = 0, brewer = "YlOrRd", revert = "no", niveles = 9, contour = "si", lon2, lat2, seq(0, 1, by = 0.2), seq(0, 1, by = 0.1), 0.7, "/salidas/ensemble/anova/pred/")
 
 
-mapa_sig(lista = pp[[3]]*pp[[4]], lista2 = pp[[4]], titulo = paste("Predictibilidad - PP",  by = "")  , nombre_fig = paste("pred_pp_01"), escala = c(0,1) 
-         ,label_escala = "", resta = 0, brewer = "PuBuGn", revert = "no", niveles = 6, contour = "si", lon2, lat2, seq(0, 1, by = 0.2),"/salidas/ensemble/anova/pred/")
+mapa_sig(lista = pp[[3]]*mask_arr, lista2 = pp[[4]], titulo = paste("Predictibilidad - PP",  by = "")  , nombre_fig = paste("pred_pp_01"), escala = c(0, 1) 
+         ,label_escala = "", resta = 0, brewer = "PuBuGn", revert = "no", niveles = 9, contour = "si", lon2, lat2, seq(0, 1, by = 0.2), seq(0, 1, by = 0.1), 0.7, "/salidas/ensemble/anova/pred/")
 
 #precip con otra escala, 0 --> 0.4
 
-mapa_sig(lista = pp[[3]]*pp[[4]], lista2 = pp[[4]], titulo = paste("Predictibilidad - PP",  by = "")  , nombre_fig = paste("pred_pp"), escala = c(0,0.5) 
-         ,label_escala = "", resta = 0, brewer = "PuBuGn", revert = "no", niveles = 5, contour = "si", lon2, lat2, seq(0, 0.4, by = 0.1),"/salidas/ensemble/anova/pred/")
+mapa_sig(lista = pp[[3]]*mask_arr, lista2 = pp[[4]], titulo = paste("Predictibilidad - PP",  by = "")  , nombre_fig = paste("pred_pp"), escala = c(0,0.5) 
+         ,label_escala = "", resta = 0, brewer = "PuBuGn", revert = "no", niveles = 9, contour = "si", lon2, lat2, seq(0, 0.5, by = 0.1), seq(0, 0.5, by = 0.1), 0.7, "/salidas/ensemble/anova/pred/")
 
