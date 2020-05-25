126ed8aeebdd0dd7859db98f56692b9073b3b9f6
Luciano Andrian <luciano.andrian@vegeta.cima.fcen.uba.ar>
2020-05-25 00:32:37 -0300

    +2 regiones

--- Correlacion_sst.R
+++ Correlacion_sst.R
@@ -52,33 +52,37 @@ pp.ens = apply(aux3, c(1,2,3,4), mean, na.rm = T)
 lats = list()
 lats[[1]] =  seq(which(lat2 == -13), which(lat2 == 2), by = 1); lats[[2]] = seq(which(lat2 == -16), which(lat2 == 4), by = 1)
 lats[[3]] = seq(which(lat2 == -16), which(lat2 == 2), by = 1); lats[[4]] = seq(which(lat2 == -26), which(lat2 == -17), by = 1)
-lats[[5]] = seq(which(lat2 == -39), which(lat2 == -24), by = 1)
+lats[[5]] = seq(which(lat2 == -39), which(lat2 == -24), by = 1); lats[[6]] = seq(which(lat2 == -7), which(lat2 == 5), by = 1)
+lats[[7]] = seq(which(lat2 == -24), which(lat2 == -15), by = 1)
+
 
 lons = list()
 lons[[1]] =  seq(which(lon2 == 291), which(lon2 == 304), by = 1); lons[[2]] = seq(which(lon2 == 301), which(lon2 == 316), by = 1)
 lons[[3]] = seq(which(lon2 == 313), which(lon2 == 326), by = 1); lons[[4]] = seq(which(lon2 == 308), which(lon2 == 321), by = 1)
-lons[[5]] = seq(which(lon2 == 296), which(lon2 == 309), by = 1)
+lons[[5]] = seq(which(lon2 == 296), which(lon2 == 309), by = 1); lons[[6]] = seq(which(lon2 == 295), which(lon2 == 325), by = 1)
+lons[[7]] = seq(which(lon2 == 305), which(lon2 == 330), by = 1)
 
-
-lats.area = list()
+lat = rev(lat)
+lats.area = list() # un poco distintas debido al grillado del mapa. 1x1 pero 1.5 2.5 3.5
 lats.area[[1]] =  seq(which(lat == -13.5), which(lat == 2.5), by = 1); lats.area[[2]] = seq(which(lat == -16.5), which(lat == 4.5), by = 1)
 lats.area[[3]] = seq(which(lat == -16.5), which(lat == 2.5), by = 1); lats.area[[4]] = seq(which(lat == -26.5), which(lat == -17.5), by = 1)
-lats.area[[5]] = seq(which(lat == -39.5), which(lat == -24.5), by = 1)
+lats.area[[5]] = seq(which(lat == -39.5), which(lat == -24.5), by = 1); lats.area[[6]] = seq(which(lat == -7.5), which(lat == 5.5), by = 1)
+lats.area[[7]] = seq(which(lat == -27.5), which(lat == -15.5), by = 1)
 
 lons.area = list()
 lons.area[[1]] =  seq(which(lon == 291.5), which(lon == 304.5), by = 1); lons.area[[2]] = seq(which(lon == 301.5), which(lon == 316.5), by = 1)
 lons.area[[3]] = seq(which(lon == 313.5), which(lon == 326.5), by = 1); lons.area[[4]] = seq(which(lon == 308.5), which(lon == 321.5), by = 1)
-lons.area[[5]] = seq(which(lon == 296.5), which(lon == 309.5), by = 1)
-
+lons.area[[5]] = seq(which(lon == 296.5), which(lon == 309.5), by = 1); lons.area[[6]] = seq(which(lon == 295.5), which(lon == 325.5), by = 1)
+lons.area[[7]] = seq(which(lon == 300.5), which(lon == 315.5), by = 1)
 
 #### graficos ####
 
 # temp
-region = c("Amazonia", "South American Monsoon", "North-estern Brazil", "SACZ", "La Plata Basin")
-region.fig = c("Am", "SAM", "NeB", "SACZ", "LPB")
+region = c("Amazonia", "South American Monsoon", "North-estern Brazil", "SACZ", "La Plata Basin", "Norte Amazonia", "South Brazil")
+region.fig = c("Am", "SAM", "NeB", "SACZ", "LPB", "NA", "SB")
 estaciones = c("MAM", "JJA", "SON", "DJF")
 
-for(i in 1:5){
+for(i in 1:7){
   area = w.mask
   area[lons.area[[i]], lats.area[[i]]] = 2
   
@@ -106,7 +110,7 @@ for(i in 1:5){
 
 
 # pp
-for(i in 1:5){
+for(i in 1:7){
   area = w.mask
   area[lons.area[[i]], lats.area[[i]]] = 2
   
